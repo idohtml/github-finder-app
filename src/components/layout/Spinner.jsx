c05d83524919ebee3e1b5e46aabd2a3de8b265ef
Jacob <Jacob.K289@outlook.com>
2023-10-07 19:13:02 +0200

    initial commit

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,16 @@
+import SpinnerImage from "../../assets/spinner.gif";
+
+const Spinner = () => {
+  return (
+    <div className="w-100 mt-20">
+      <img
+        className="text-center mx-auto"
+        width={180}
+        src={SpinnerImage}
+        alt="Loading..."
+      />
+    </div>
+  );
+};
+
+export default Spinner;
---
+++ src/context/github/GithubContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+import PropTypes from "prop-types";
+
+const GithubContext = createContext();
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  return (
+    <GithubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
+
+GithubProvider.propTypes = {
+  children: PropTypes.node,
+};
